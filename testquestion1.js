d9aab05ff02fa1af11c818b9fd531fc7f622f827
jovanohks <jovanoh400@gmail.com>
2026-09-03 11:46:19 +0000

    Sep 3, 2026, 11:46 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,25 @@
+let width=600;
+let height=400;
+let player;
+let floor;
 
+
+
+function setup(){
+    new Canvas(600,400);
+    player=new Sprite();
+    player.width=30;
+    player.height=30;
+
+    floor=new Sprite();
+    floor.width=2400;
+    floor.height=20;
+
+    floor.y=400;
+    floor.collider="static";
+    world.gravity.y=10;
+}
+
+function draw(){
+    background(255);
+}
